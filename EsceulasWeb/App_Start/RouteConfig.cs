ea9b3b6b4f386ab114aaa330ad76b620ccad6d40
Ivan <ivan.peters.v@gmail.com>
2017-12-15 19:11:09 -0300

    init

--- EsceulasWeb/App_Start/RouteConfig.cs
+++ EsceulasWeb/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace EsceulasWeb
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Listado", id = UrlParameter.Optional }
             );
         }
     }
--- EsceulasWeb/Controllers/HomeController.cs
+++ EsceulasWeb/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EsceulasWeb.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,23 +9,55 @@ namespace EsceulasWeb.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        ESCUELASEntities1 cnx;
+       
+        public HomeController()
         {
+          cnx = new ESCUELASEntities1();
+
+        }
+        public ActionResult Formulario()
+        {
+
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Listado()
         {
-            ViewBag.Message = "Your application description page.";
 
-            return View();
+            cnx.Database.Connection.Open();
+
+            List<PROFESOR> profesores = cnx.PROFESOR.ToList();
+            cnx.Database.Connection.Close();
+            return View(  profesores );
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
+        public ActionResult Guardar( string rut, string nombre, string titulo, string grado) {
 
-            return View();
+            return View("Listado");
+        }
+
+
+        public ActionResult Ficha(string rut, string nombre, string titulo, string grado)
+        {
+            PROFESOR profe = new PROFESOR()
+            {
+                Rut = rut,
+                Nombre = nombre,
+                Titulo = titulo,
+                Grado = grado
+            };
+
+            cnx.PROFESOR.Add(profe);
+            cnx.SaveChanges();
+            return View(profe);
+        }
+
+        public ActionResult Ver(string rut)
+        {
+            
+
+            return View("Ficha", null);
         }
     }
-}
+    }
